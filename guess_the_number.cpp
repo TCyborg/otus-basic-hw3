4d2b93758e7c9a2d987c424ffef37bb3711e672b
Тимонин Алексей Юрьевич <a.timonin@itelma.su>
2025-05-19 08:20:05 +0000

    add opt 1,2

--- guess_the_number.cpp
+++ guess_the_number.cpp
@@ -6,13 +6,50 @@
 #include <fstream>
 #include <string>
 
-int main() {
+const std::string high_scores_filename = "high_scores.txt";
 
-	const int target_value = generate_random_value(100);
+int main(int argc, char** argv) {
+	int max_value = 100;
 	int current_value = 0;
 	bool not_win = true;
 	int attempts_count = 0;
-		const std::string high_scores_filename = "high_scores.txt";
+
+	// argc - it is a count of arguments
+	// But there is some detail - OS always pass one system argument - the name of the executable
+	// So, is the application was executed without arguments at all, argc will be still == 1
+
+	// Let's print this argument
+	if (argc > 0) {
+		std::cout << "We have at least one argument!" << std::endl;
+		std::cout << "argv[0] = " << argv[0] << std::endl;
+	}
+
+	// To check - does use print some other argument we should check if the argc >= 2
+	if (argc >= 2) {
+		std::cout << "We have one more argument!" << std::endl;
+		std::cout << "argv[1] = " << argv[1] << std::endl;
+
+		std::string arg1_value{ argv[1] };
+		if (arg1_value == "-max") {
+			std::cout << "-parameter max was detected!" << std::endl;
+
+			// We've detected the '-parameter' argument. And we extect that after this argument there is a value:
+			if (argc < 3) {
+				std::cout << "Wrong usage! The argument '-parameter' requires some value!" << std::endl;
+				return -1;
+			}
+			// We need to parse the string to the int value
+			max_value = std::stoi(argv[2]);
+			std::cout << "The '-parameter' value = " << max_value << std::endl;
+		}
+		if (arg1_value == "-table") {
+			print_leaderboard();
+			return 0;
+		}
+	}
+
+	const int target_value = generate_random_value(max_value);
+
 	// Ask about name
 	std::cout << "Hi! Enter your name, please:" << std::endl;
 	std::string user_name;
@@ -51,34 +88,7 @@ int main() {
 		out_file << std::endl;
 	} // end of score here just to mark end of the logic block of code
 
-	// Read the high score file and print all results
-	{
-		std::ifstream in_file{high_scores_filename};
-		if (!in_file.is_open()) {
-			std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
-			return -1;
-		}
-
-		std::cout << "High scores table:" << std::endl;
-
-		std::string username;
-		int high_score = 0;
-		while (true) {
-			// Read the username first
-			in_file >> username;
-			// Read the high score next
-			in_file >> high_score;
-			// Ignore the end of line symbol
-			in_file.ignore();
-
-			if (in_file.fail()) {
-				break;
-			}
-
-			// Print the information to the screen
-			std::cout << username << '\t' << high_score << std::endl;
-		}
-	}
+	print_leaderboard();
 
 	return 0;
 }
@@ -89,4 +99,34 @@ const int generate_random_value(const int max_value){
 	const int random_value = std::rand() % max_value;
 
 	return random_value;
+}
+
+int print_leaderboard()
+	// Read the high score file and print all results
+{
+	std::ifstream in_file{high_scores_filename};
+	if (!in_file.is_open()) {
+		std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
+		return -1;
+	}
+
+	std::cout << "High scores table:" << std::endl;
+
+	std::string username;
+	int high_score = 0;
+	while (true) {
+		// Read the username first
+		in_file >> username;
+		// Read the high score next
+		in_file >> high_score;
+		// Ignore the end of line symbol
+		in_file.ignore();
+
+		if (in_file.fail()) {
+			break;
+		}
+
+		// Print the information to the screen
+		std::cout << username << '\t' << high_score << std::endl;
+	}
 }
--- guess_the_number.h
+++ guess_the_number.h
@@ -1,3 +1,4 @@
 #pragma once
 
 const int generate_random_value(const int max_value);
+int print_leaderboard(void);
